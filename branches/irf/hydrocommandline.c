355df79f07abf2a18ab794abfe36aa1d373120da
huttone <huttone@d851157f-c847-0410-9c9b-b1141f3f3998>
2009-02-10 02:10:53 +0000

    * Clean up command line interface.  Add long options.     -V, --verbose for verbose     -v, --version for version number     -h, --help for help message     -i, --in-file=FILE for input file     -o, --out-dir=DIR for output directory   Input file and output directory can still be given as command   line arguments as before.

--- branches/irf/hydrocommandline.c
+++ branches/irf/hydrocommandline.c
@@ -14,99 +14,204 @@
  *-------------------------------------------------------------------------------------------*/
 
 #include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
 #include "hydroclimate.h"
 #include "hydroinout.h"
 #include "hydroparams.h"
 #include "hydroalloc_mem.h"
+#include "hydrotrend.h"
 #define MAXLENGTH (80)
- 
-/*-----------------------------
- *  Start of HydroCommandLine
- *-----------------------------*/
-int hydrocommandline(int *argc,char **argv)
-{ 
 
-/*-------------------
- *  Local Variables
- *-------------------*/
-int err, jj;
+#define HT_DEFAULT_PREFIX  "HYDRO"
+#define HT_DEFAULT_OUT_DIR "HYDRO_OUT"
 
-/*------------------------
- *  Initialize Variables
- *------------------------*/
-err = 0;
+#define TRUE  (1)
+#define FALSE (0)
 
-	for (jj=0; jj<*argc; jj++){
-		strcpy(commandlinearg[jj],argv[jj]);
-	}
+int is_valid_str (char *str);
+char* to_upper_str (char *str);
 
-	if (*argc == 1){
-   /*---------------------------------------------------
-    *  argc = 1 creats standard output filenames named
-    *  HYDRO.*
-    *---------------------------------------------------*/
-		strcpy(commandlinearg[1],DUMMY);
-		fprintf(stderr, "\n  Hydrotrend started without given file name. Output will \n");
-		fprintf(stderr, "  be written to standard output files, named %s.\n",commandlinearg[1]);
-		fprintf(stderr, "  Old files will be overwritten!!\n\n");
-	}
-	
-	if (*argc == 2)
-   /*------------------------------------------------
-    *  argc = 2 makes it possible to make different
-    *  filenames per each run.
-    *------------------------------------------------*/
-		for( jj=0; jj<MAXLENGTH; jj++ ){
-			if (commandlinearg[1][jj] == '%' || commandlinearg[1][jj] == '*' || commandlinearg[1][jj] == '#'
-					|| commandlinearg[1][jj] == '@' || commandlinearg[1][jj] == '-' || commandlinearg[1][jj] == '^'
-					|| commandlinearg[1][jj] == '"' || commandlinearg[1][jj] == '?' || commandlinearg[1][jj] == '!'
-					|| commandlinearg[1][jj] == '.' || commandlinearg[1][jj] == ',' || commandlinearg[1][jj] == '$'){
-				fprintf(stderr, "  HydroTrend ERROR: Incorrect command line \n");
-				fprintf(stderr, "    You can use only characters and numbers to name the project. \n");
-				fprintf(stderr, "Don't use %c.\n",commandlinearg[1][jj]);
-				err++;
-			}
-			commandlinearg[1][jj]=toupper(commandlinearg[1][jj]);
-		}
-		
-	if (*argc == 3){
-   /*------------------------------------------------
-    *  argc = 3 makes it possible to display all the
-    *  subroutines the program goes through
-    *------------------------------------------------*/
-		for( jj=0; jj<MAXLENGTH; jj++ ){
-			if (commandlinearg[1][jj] == '%' || commandlinearg[1][jj] == '*' || commandlinearg[1][jj] == '#'
-					|| commandlinearg[1][jj] == '@' || commandlinearg[1][jj] == '-' || commandlinearg[1][jj] == '^'
-					|| commandlinearg[1][jj] == '"' || commandlinearg[1][jj] == '?' || commandlinearg[1][jj] == '!'
-					|| commandlinearg[1][jj] == '.' || commandlinearg[1][jj] == ',' || commandlinearg[1][jj] == '$'){
-				fprintf(stderr, "  HydroTrend ERROR: Incorrect command line \n");
-				fprintf(stderr, "    You can use only characters and numbers to name the project. \n");
-				fprintf(stderr, "Don't use %c.\n",commandlinearg[1][jj]);
-				err++;
-			}
-			commandlinearg[1][jj]=toupper(commandlinearg[1][jj]);
-		}
-		for ( jj=0; jj<MAXLENGTH; jj++ ){
-			commandlinearg[2][jj]=toupper(commandlinearg[2][jj]);
-		}
-    	if (strncmp(commandlinearg[2],verbosearg,2) == 0){
-    		verbose = 1;
-		}
-	}
-    
-	if (*argc >3){
-   /*-----------------------------------------------
-    *  argc > 4 does not exist. It will handle the
-    *  output as if argc = 1.
-    *-----------------------------------------------*/
-		strcpy(commandlinearg[1],DUMMY);
-		fprintf(stderr, "  HydroTrend ERROR: Incorrect command line \n");
-		fprintf(stderr, "    argc should equal 1 or 2 \n");
-		fprintf(stderr, "    argc = %d \n", *argc);
-		fprintf(stderr, "    HydroTrend does not use more than 2 command line arguments. \n");
-		fprintf(stderr, "    Ignoring all arguments. \n");
-		fprintf(stderr, "    Output will be written to standard output files, named %s.\n",commandlinearg[1]);
-	}
+static int verbose_flag = 0;
+static int version_flag = 0;
+static int help_flag    = 0;
 
-	return(err);
-} /* HydroCommandLine.c */ 
+static struct option ht_long_opts[] =
+{
+   { "verbose", no_argument,       &verbose_flag,  1  },
+   { "brief",   no_argument,       &verbose_flag,  0  },
+   { "version", no_argument,       &version_flag,  1  },
+   { "help",    no_argument,       &help_flag,     1  },
+   { "in-file", required_argument, NULL,          'i' },
+   { "out-dir", required_argument, NULL,          'o' },
+   { NULL     , 0,                 NULL,           0  }
+};
+
+static char* help_msg[] =
+{
+  "Usage: hydrotrend [options] [prefix [directory]]",
+  "Options:",
+  "  -V or --verbose      Be verbose",
+  "  --brief              Be terse",
+  "  -v or --version      Print version number and exit",
+  "  -?,-h or --help      Print this information and exit",
+  "  -i, --in-file=PREFIX Use PREFIX.IN as input file",
+  "  -o, --out-dir=DIR    Put output in directory DIR",
+  NULL
+};
+
+/** Parse command line options for hydrotrend.
+
+Usage: hydrotrend [options] [prefix [directory]]
+
+file is the prefix for the HydroTrend output files.  If not given, HYDRO is
+used.  If directory is given output files will be written to that directory.
+If not given, HYDRO_OUT is used.
+
+Use: 'hydrotrend --help' for a full list of command line options.
+*/
+int
+parse_command_line( int argc, char* argv[] )
+{
+  int success = TRUE;
+
+  if ( argv )
+  {
+    int ch;
+    char* in_file_prefix = NULL;
+    char* out_dir        = NULL;
+    int option_index;
+
+    while ( (ch = getopt_long(argc, argv, "vVh?i:o:", ht_long_opts, &option_index)) != -1 )
+      {
+        switch (ch)
+          {
+            case 'i':
+              in_file_prefix = strdup( optarg );
+              break;
+            case 'o':
+              out_dir = strdup( optarg );
+              break;
+            case 'v':
+              version_flag = 1;
+              break;
+            case 'V':
+              verbose_flag = 1;
+              break;
+            case '?':
+            case 'h':
+              help_flag = 1;
+              break;
+            case 0:
+              break;
+            default:
+              fprintf( stderr, "Error: Unknown option %d\n", ch );
+              exit( EXIT_FAILURE );
+          }
+      }
+
+    if ( help_flag )
+      {
+        char** str;
+        for ( str=help_msg; *str; str++ )
+          fprintf ( stdout, "%s\n", *str );
+        exit( EXIT_SUCCESS );
+      }
+
+    if ( version_flag )
+      {
+        fprintf( stdout, "HydroTrend version %d.%d.%d\n",
+                 HT_MAJOR_VERSION, HT_MINOR_VERSION, HT_MICRO_VERSION );
+        exit( EXIT_SUCCESS );
+      }
+
+    if ( optind < argc )
+      {
+        if ( !in_file_prefix )
+          in_file_prefix = strdup( argv[optind++] );
+        else
+          {
+            fprintf( stderr, "Error: Prefix specifed twice\n", ch );
+            exit( EXIT_FAILURE );
+          }
+      }
+    if ( !in_file_prefix )
+      in_file_prefix = strdup ( HT_DEFAULT_PREFIX );
+
+    if ( optind < argc )
+      {
+fprintf( stderr, "optind = %d\n", optind );
+        if ( !out_dir )
+          out_dir = strdup( argv[optind++] );
+        else
+          {
+            fprintf( stderr, "Error: Output directory specifed twice\n", ch );
+            exit( EXIT_FAILURE );
+          }
+      }
+    if ( !out_dir )
+      out_dir = strdup ( HT_DEFAULT_OUT_DIR );
+
+
+    if ( verbose_flag )
+      verbose = 1;
+    else
+      verbose = 0;
+
+    if (    !is_valid_str ( in_file_prefix )
+         || !is_valid_str ( out_dir ) )
+      {
+        fprintf (stderr, "  HydroTrend ERROR: Incorrect command line \n");
+        fprintf (stderr,
+                "    Prefix/directory can only contain alpha numeric characters\n" );
+        exit( EXIT_FAILURE );
+      }
+    else
+      {
+        to_upper_str ( in_file_prefix );
+        to_upper_str ( out_dir );
+      }
+
+    if ( verbose )
+      {
+        fprintf( stderr, "Using input file %s.IN\n", in_file_prefix );
+        fprintf( stderr, "Using output directory %s\n", out_dir );
+      }
+
+    strcpy ( commandlinearg[1], in_file_prefix );
+    strcpy ( commandlinearg[2], out_dir        );
+
+    free( in_file_prefix );
+    free( out_dir        );
+  }
+
+   return success;
+}
+
+int
+is_valid_str (char *str)
+{
+  int is_valid = 1;
+
+  if (strchr (str, '%') || strchr (str, '*') ||
+      strchr (str, '#') || strchr (str, '@') ||
+      strchr (str, '-') || strchr (str, '^') ||
+      strchr (str, '"') || strchr (str, '?') ||
+      strchr (str, '!') || strchr (str, '.') ||
+      strchr (str, ',') || strchr (str, '$'))
+    is_valid = 0;
+
+  return is_valid;
+}
+
+char *
+to_upper_str (char *str)
+{
+  if (str)
+    {
+      int i;
+      for (i = 0; i < strlen (str); i++)
+        str[i] = toupper (str[i]);
+    }
+  return str;
+}
