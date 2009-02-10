42df5a6a4e24ed59ec83d063cff89b24dc1fb38f
huttone <huttone>
2009-02-10 02:38:26 +0000

    * Minor formatting * Add macros HT_MAJOR_VERSION, HT_MINOR_VERSION, HT_MICRO_VERSION

--- hydrotrend.h
+++ hydrotrend.h
@@ -14,42 +14,46 @@
 #define HYDROTREND_H_
 #include "hydroreadclimate.h"
 
-int hydrocommandline(int *argc, char **argv);
-void hydrosetparams();
-int hydrosecurityinputcheck();
-int hydroreadinput();
-int hydroreadhypsom();
-int hydroreadclimate(gw_rainfall_etc *gw_rain);
-int hydrosetglobalpar();
-int hydrocheckinput();
-int hydroopenfiles();
-int hydrosetgeoparams( gw_rainfall_etc *gw_rain );
-int hydrorandom();
-int hydroshoulder();
-int hydroclimate(gw_rainfall_etc *gw_rain);
-int hydroweather(gw_rainfall_etc *gw_rain);
-int hydrohypsom();
-int hydroglacial();
-int hydrosnow();
-int hydrorain();
-int hydrosumflow();
-int hydromaxevents();
-int hydrosedload ( gw_rainfall_etc *gw_rain );
-int hydrooutput();
-int hydroprintannual();
-int hydrocalqsnew();
-int hydroprintstat();
-int hydroswap();
-int hydrooutletfraction(int x);
-int hydrosetnumberoutlet(int x);
-int hydroqfractionshuffle( int k );
-void hydroallocmemoutlet(int ep, int nepochs);
-void hydroallocmemoutlet1(int ep, int nepochs);
-void hydrofreememoutlet(int j);
-void hydrofreememoutlet1(int ep);
-int hydroshuffle(int dvals[31],int mnth);
-int hydroexpdist(double pvals[31],int mnth);
-void hydroinputalloc(int ep);
+#define HT_MAJOR_VERSION (3)
+#define HT_MINOR_VERSION (0)
+#define HT_MICRO_VERSION (5)
 
-#endif
+int hydrocommandline (int *argc, char **argv);
+void hydrosetparams ();
+int hydrosecurityinputcheck ();
+int hydroreadinput ();
+int hydroreadhypsom ();
+int hydroreadclimate (gw_rainfall_etc * gw_rain);
+int hydrosetglobalpar ();
+int hydrocheckinput ();
+int hydroopenfiles ();
+int hydrosetgeoparams (gw_rainfall_etc * gw_rain);
+int hydrorandom ();
+int hydroshoulder ();
+int hydroclimate (gw_rainfall_etc * gw_rain);
+int hydroweather (gw_rainfall_etc * gw_rain);
+int hydrohypsom ();
+int hydroglacial ();
+int hydrosnow ();
+int hydrorain ();
+int hydrosumflow ();
+int hydromaxevents ();
+int hydrosedload (gw_rainfall_etc * gw_rain);
+int hydrooutput ();
+int hydroprintannual ();
+int hydrocalqsnew ();
+int hydroprintstat ();
+int hydroswap ();
+int hydrooutletfraction (int x);
+int hydrosetnumberoutlet (int x);
+int hydroqfractionshuffle (int k);
+void hydroallocmemoutlet (int ep, int nepochs);
+void hydroallocmemoutlet1 (int ep, int nepochs);
+void hydrofreememoutlet (int j);
+void hydrofreememoutlet1 (int ep);
+int hydroshuffle (int dvals[31], int mnth);
+int hydroexpdist (double pvals[31], int mnth);
+void hydroinputalloc (int ep);
 
+#endif /*  */
+  
