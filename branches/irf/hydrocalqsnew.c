7b01b95b559d7bec1958eb37e94a6ed06c525669
huttone <huttone@d851157f-c847-0410-9c9b-b1141f3f3998>
2009-02-12 16:57:49 +0000

    * Remove extra new-line characters

--- branches/irf/hydrocalqsnew.c
+++ branches/irf/hydrocalqsnew.c
@@ -13,51 +13,49 @@
  *  Qsgrandtotaloutlettot Hydrocalqsnew.c double kg/s total Qs of all outlets
  *
  *-------------------------------------------------------------------------------------------*/
- 
+
 #include <stdio.h>
 #include "hydroparams.h"
 #include "hydrotimeser.h"
 #include "hydroclimate.h"
 #include "hydroinout.h"
-  
+
 /*----------------------
  *  Start main program
- *----------------------*/ 
-  int
-hydrocalqsnew (void) 
+ *----------------------*/
+int
+hydrocalqsnew (void)
 {
-  
+
 /*-------------------
  *  Local Variables
- *-------------------*/ 
+ *-------------------*/
   int err, p;
-  double Qsgrandtotaloutlettot;
-  
+  double Qsgrandtotaloutlettot;
+
 /*------------------------
  *  Initialize variables
- *------------------------*/ 
-    err = 0;
-  Qsgrandtotaloutlettot = 0.0;
-  
+ *------------------------*/
+  err = 0;
+  Qsgrandtotaloutlettot = 0.0;
+
 /*--------------------------------------------------
  *  Calculate mean Qs and calculate the difference
  *  between mean Qs and (Qsbar+ glacier part).
- *--------------------------------------------------*/ 
-    if (Qsglacierbar[ep] > 0.0)
+ *--------------------------------------------------*/
+  if (Qsglacierbar[ep] > 0.0)
     {
-      Qsmean[ep] = Qspsigrandtotal[ep] / (nyears[ep] * daysiy * dTOs);
-      Qsbarnew[ep] =
+      Qsmean[ep] = Qspsigrandtotal[ep] / (nyears[ep] * daysiy * dTOs);
+      Qsbarnew[ep] =
         ((1.0 - sedfilter[ep]) * (Qsbartot[ep] +
                                   (fractionglaciersediment[ep] *
                                    Qsglacierbar[ep]))) / Qsmean[ep];
-    }
-  
+    }
+
   else
     {
-      Qsmean[ep] = Qsgrandtotal[ep] / (nyears[ep] * daysiy * dTOs);
-      Qsbarnew[ep] = ((1.0 - sedfilter[ep]) * (Qsbartot[ep])) / Qsmean[ep];
-    }
-  return (err);
-}                             /* end of Hydrocalqsnew */
-
-
+      Qsmean[ep] = Qsgrandtotal[ep] / (nyears[ep] * daysiy * dTOs);
+      Qsbarnew[ep] = ((1.0 - sedfilter[ep]) * (Qsbartot[ep])) / Qsmean[ep];
+    }
+  return (err);
+}                               /* end of Hydrocalqsnew */
