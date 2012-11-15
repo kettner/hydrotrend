f4d445c4c29eb6ead8c13d7d8735cc0ad9498006
huttone <huttone>
2012-11-15 20:08:51 +0000

    * Update CMakeLists.txt; Install hydrotrend.pc
      Update CMakeLists.txt:
        * Build shared libraries by default
        * Install hydrotrend.pc under lib/pkgconfig
        * Install bmi_hydrotrend.h

--- hydrotrend.pc.cmake
+++ hydrotrend.pc.cmake
@@ -1,5 +1,6 @@
-Name: hydrotrend
-Description: Hydrotrend
+Name: LibHydrotrend
+Description: Hydrotrend library
 Version: ${HYDROTREND_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lhydrotrend
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
+
