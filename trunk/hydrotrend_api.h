81598851f10beed8008c930b49c30827d9c0fbab
huttone <huttone@d851157f-c847-0410-9c9b-b1141f3f3998>
2013-02-13 23:47:53 +0000

    * Rename hydrotrend BMI namespace from BMI_ to BMI_HYDROTREND_.
      To avoid naming conflicts, the hydrotrend BMI namespace is now
      BMI_HYDROTREND, rather than just BMI_.

--- trunk/hydrotrend_api.h
+++ trunk/hydrotrend_api.h
@@ -22,9 +22,9 @@ typedef ht_state BMI_Model;
 #define BMI_SUCCESS (0)
 #define BMI_FAILURE (1)
 
-#define BMI_COMPONENT_NAME_MAX (2048)
-#define BMI_VAR_NAME_MAX (2048)
-#define BMI_UNIT_NAME_MAX (2048)
+#define BMI_HYDROTREND_COMPONENT_NAME_MAX (2048)
+#define BMI_HYDROTREND_VAR_NAME_MAX (2048)
+#define BMI_HYDROTREND_UNIT_NAME_MAX (2048)
 
 typedef enum {
   BMI_VAR_TYPE_UNKNOWN = 0,
@@ -50,20 +50,21 @@ typedef enum {
 }
 BMI_Grid_type;
 
-int BMI_Initialize (const char *, BMI_Model **);
-int BMI_Update_until (BMI_Model *, double);
-int BMI_Update (BMI_Model * s);
-int BMI_Finalize (BMI_Model *);
+int BMI_HYDROTREND_Initialize (const char *, BMI_Model **);
+int BMI_HYDROTREND_Update_until (BMI_Model *, double);
+int BMI_HYDROTREND_Update (BMI_Model * s);
+int BMI_HYDROTREND_Finalize (BMI_Model *);
 
-int BMI_Get_component_name (BMI_Model * s, char *name);
-int BMI_Get_output_var_names (BMI_Model * s, char **names);
-int BMI_Get_output_var_name_count (BMI_Model * s, int *output_var_count);
-int BMI_Get_input_var_names (BMI_Model * s, char **names);
-int BMI_Get_input_var_name_count (BMI_Model * s, int *input_var_count);
+int BMI_HYDROTREND_Get_component_name (BMI_Model * s, char *name);
+int BMI_HYDROTREND_Get_output_var_names (BMI_Model * s, char **names);
+int BMI_HYDROTREND_Get_output_var_name_count (BMI_Model * s, int *output_var_count);
+int BMI_HYDROTREND_Get_input_var_names (BMI_Model * s, char **names);
+int BMI_HYDROTREND_Get_input_var_name_count (BMI_Model * s, int *input_var_count);
 
-int BMI_Get_var_type (BMI_Model * s, const char *name, BMI_Var_type *type);
-int BMI_Get_var_rank (BMI_Model * s, const char *name, int *rank);
-int BMI_Get_var_point_count (BMI_Model * s, const char *name, int *count);
+int BMI_HYDROTREND_Get_var_type (BMI_Model * s, const char *name, BMI_Var_type *type);
+int BMI_HYDROTREND_Get_var_rank (BMI_Model * s, const char *name, int *rank);
+int BMI_HYDROTREND_Get_var_stride (BMI_Model * s, const char *name, int *stride);
+int BMI_HYDROTREND_Get_var_point_count (BMI_Model * s, const char *name, int *count);
 
 ht_state *ht_initialize (char* in_dir, char* prefix, char* dir);
 //ht_state *ht_finalize (ht_state * s);
@@ -72,24 +73,24 @@ ht_state *ht_initialize (char* in_dir, char* prefix, char* dir);
 const char** ht_get_exchange_items (void);
 double ht_get_value (ht_state * s, char* value);
 
-int BMI_Get_double (BMI_Model * s, const char * val_s, double *dest);
-int BMI_Get_double_ptr (BMI_Model *s, const char *name, double **dest);
+int BMI_HYDROTREND_Get_double (BMI_Model * s, const char * val_s, double *dest);
+int BMI_HYDROTREND_Get_double_ptr (BMI_Model *s, const char *name, double **dest);
 
-int BMI_Get_grid_shape (BMI_Model * s, const char * val_s, int * shape);
-int BMI_Get_grid_spacing (BMI_Model * s, const char * val_s, double * spacing);
-int BMI_Get_grid_origin (BMI_Model * s, const char * val_s, double * origin);
+int BMI_HYDROTREND_Get_grid_shape (BMI_Model * s, const char * val_s, int * shape);
+int BMI_HYDROTREND_Get_grid_spacing (BMI_Model * s, const char * val_s, double * spacing);
+int BMI_HYDROTREND_Get_grid_origin (BMI_Model * s, const char * val_s, double * origin);
 
-int BMI_Get_current_time (BMI_Model *, double *);
-int BMI_Get_start_time (BMI_Model *, double *);
-int BMI_Get_end_time (BMI_Model *, double *);
-int BMI_Get_time_units (BMI_Model *s, char *units);
+int BMI_HYDROTREND_Get_current_time (BMI_Model *, double *);
+int BMI_HYDROTREND_Get_start_time (BMI_Model *, double *);
+int BMI_HYDROTREND_Get_end_time (BMI_Model *, double *);
+int BMI_HYDROTREND_Get_time_units (BMI_Model *s, char *units);
 
-#define NO_BMI_SET_DOUBLE
-#define NO_BMI_GET_GRID_CONNECTIVITY
-#define NO_BMI_GET_GRID_OFFSET
-#define NO_BMI_GET_GRID_X
-#define NO_BMI_GET_GRID_Y
-#define NO_BMI_GET_GRID_Z
+#define NO_BMI_HYDROTREND_SET_DOUBLE
+#define NO_BMI_HYDROTREND_GET_GRID_CONNECTIVITY
+#define NO_BMI_HYDROTREND_GET_GRID_OFFSET
+#define NO_BMI_HYDROTREND_GET_GRID_X
+#define NO_BMI_HYDROTREND_GET_GRID_Y
+#define NO_BMI_HYDROTREND_GET_GRID_Z
 
 double ht_get_velocity (ht_state * s);
 double ht_get_width (ht_state * s);
