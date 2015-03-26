c42d7a0a4b83695f18b55ab88143d4d9d7b7eb9f
mcflugen <mcflugen@gmail.com>
2015-03-26 17:25:36 -0600

    Change include path for cflags.

--- hydrotrend.pc.cmake
+++ hydrotrend.pc.cmake
@@ -2,5 +2,5 @@ Name: LibHydrotrend
 Description: Hydrotrend library
 Version: ${HYDROTREND_VERSION}
 Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmi_hydrotrend
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include/hydrotrend
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
 
