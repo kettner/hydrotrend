f4abfc8b8a36e3fa47e1e104ba89ba98aa1f7cc2
mcflugen <mcflugen@gmail.com>
2015-03-26 16:09:30 -0600

    Declare new getter functions.

--- hydrotrend_irf.h
+++ hydrotrend_irf.h
@@ -25,6 +25,23 @@ extern state * hydro_initialize (char* in_dir, char *in_file_prefix,
 extern state * hydro_run (state * s, double time);
 extern state * hydro_finalize (state * s);
 
+extern double * hydro_get_velocity_ptr (state * self);
+extern double hydro_get_velocity (state * self);
+extern double * hydro_get_width_ptr (state * self);
+extern double hydro_get_width (state * self);
+extern double * hydro_get_depth_ptr (state * self);
+extern double hydro_get_depth (state * self);
+extern double * hydro_get_water_discharge_ptr (state * self);
+extern double hydro_get_water_discharge (state * self);
+extern double * hydro_get_sediment_discharge_ptr (state * self);
+extern double hydro_get_sediment_discharge (state * self);
+extern double * hydro_get_bedload_flux_ptr (state * self);
+extern double hydro_get_bedload_flux (state * self);
+extern double * hydro_get_precipitation_ptr (state * self);
+extern double hydro_get_precipitation (state * self);
+extern double * hydro_get_temperature_ptr (state * self);
+extern double hydro_get_temperature (state * self);
+
 #if defined(__cplusplus)
 }
 #endif
